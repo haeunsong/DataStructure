261cfe4f0f2ad804c65b1028efd3e676109c494c
haeunsong <62704334+haeunsong@users.noreply.github.com>
2020-08-23 18:35:23 +0900

    class Term

---
+++ Java/Polynomial/Term.java
@@ -0,0 +1,17 @@
+public class Term {
+  public int coef; // 계수
+  public int expo; // 차수
+  
+  public Term(int c,int e){
+    coef = c;
+    expo = e;
+  }
+  
+  public int calc(int x){
+    return coef * (int)Math.pow(x,expo);
+  }
+  
+  public String toString(){
+    return coef + "X^" + expo;
+  }
+}
